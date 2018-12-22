f1cf4a61ee492b1fe66544c8e4c337968b53dc20
“l <18392601384@163.com>
2018-12-22 22:55:00 +0800

    删除数组中重复某一key关键字

--- src/main.go
+++ src/main.go
@@ -4,4 +4,23 @@ import "fmt"
 
 func main(){
 	fmt.Println("hello go!")
+	var a []int = []int{1,2,1,5}
+	key := 1
+	a = test(a,key)
+	fmt.Println(a)
+}
+
+func test(a []int,key int)([]int){
+	i := 0
+	for i<len(a) {
+		if a[i] != key {
+			i++
+		} else {
+			for j := i;j<len(a)-1;j++ {
+				a[j] = a[j+1]
+			}
+			a = a[:len(a)-1]
+		}
+	}
+	return a
 }
